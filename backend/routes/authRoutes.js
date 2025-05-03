d4890383fb4c5c9f9bfc8c9d7447a3dc78f886ed
Amancore__ <aman8804623822@gmail.com>
2025-05-03 22:18:58 +0530

    ok

--- backend/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const authController = require('../controllers/authcontroller');
-const middleware=require("../middleware/authMiddleware");
-const router = express.Router();
-
-router.post('/register', authController.register);
-router.post('/login',middleware.usercheck,authController.login);
-
-module.exports = router;
